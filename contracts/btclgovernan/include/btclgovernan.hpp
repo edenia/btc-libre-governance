b7bca494c3eae3d823b8d2818bd9f3673cfdd8f7
Adriel Diaz <adriexnet@gmail.com>
2021-10-19 19:47:31 -0600

    feat: allow create proposals

--- contracts/btclgovernan/include/btclgovernan.hpp
+++ contracts/btclgovernan/include/btclgovernan.hpp
@@ -33,19 +33,87 @@ public:
      * 
      * @return no return value.
      */
-   ACTION setparams(uint8_t vote_threshold, name funding_account, uint8_t voting_days, asset proposal_cost, name approver);
+   ACTION setparams(name funding_account, uint8_t vote_threshold, uint8_t voting_days, asset minimum_balance_to_create_proposals, asset proposal_cost, name approver);
+
+   /**
+     * crea
+     * 
+     * This action set the params to be used on the voting process.
+     *
+     * @param name
+     * @param title
+     * @param detail
+     * @param amount
+     * @param url
+     * @param receiver
+     * @param creator
+     * 
+     * @return no return value.
+     */
+   ACTION create(name creator, name receiver, name name, string title, string detail, asset amount, string url);
+
+   bool is_valid_minimum_balance(name account);
 
    ACTION clear();
 
 private:
+   string EOSIO_TOKEN_CONTRACT = "eosio.token";
+   
+   string SUPPORTED_TOKEN = "XPR";
+
+   enum proposal_status : uint8_t
+   {
+      DRAFT = 1,
+      ACTIVE = 2,
+      SUCCEEDED = 3,
+      DEFEATED = 4,
+      CANCELED = 5,
+      COMPLETED = 6,
+   };
+
    TABLE params
    {
-      uint8_t vote_threshold;
       name funding_account;
+      uint8_t vote_threshold;
       uint8_t voting_days;
+      asset minimum_balance_to_create_proposals;
       asset proposal_cost;
       name approver;
    };
    typedef singleton<name("params"), params> params_table;
 
+   TABLE proposals
+   {
+      name creator;
+      name receiver;
+      name name;
+      string title;
+      string detail;
+      asset amount;
+      string url;
+      time_point_sec created_on;
+      time_point_sec expires_on;
+      uint8_t votes_for;
+      uint8_t votes_against;
+      uint8_t status;
+
+      uint64_t primary_key() const { return name.value; }
+   };
+   typedef multi_index<name("proposals"), proposals> proposals_table;
+
+   struct stats {
+      asset supply;
+      asset max_supply;
+      name issuer;
+
+      uint64_t primary_key() const { return supply.symbol.code().raw(); }
+   };
+   typedef multi_index<name("stat"), stats> stats_table;
+
+   struct account {
+      asset balance;
+
+      uint64_t primary_key() const { return balance.symbol.code().raw(); }
+   };
+   typedef multi_index<name("accounts"), account> account_table;
 };
--- contracts/btclgovernan/src/btclgovernan.cpp
+++ contracts/btclgovernan/src/btclgovernan.cpp
@@ -1,32 +1,89 @@
 /*
-*
-* @author  EOSCostaRica.io [https://eoscostarica.io]
-*
-* @section DESCRIPTION
-*  Source file for the actions related with the btclgovernan contract
-*
-*  GitHub: https://github.com/eoscostarica/btc-libre-governance
-*
-*/
+ *
+ * @author  EOSCostaRica.io [https://eoscostarica.io]
+ *
+ * @section DESCRIPTION
+ *  Source file for the actions related with the btclgovernan contract
+ *
+ *  GitHub: https://github.com/eoscostarica/btc-libre-governance
+ *
+ */
 
 #include <btclgovernan.hpp>
 #include <eosio/system.hpp>
+#include <eosio/system.hpp>
 
-ACTION btclgovernan::setparams(uint8_t vote_threshold, name funding_account, uint8_t voting_days, asset proposal_cost, name approver)
-{
+ACTION btclgovernan::setparams(name funding_account, uint8_t vote_threshold, uint8_t voting_days, asset minimum_balance_to_create_proposals, asset proposal_cost, name approver) {
   require_auth(get_self());
+
+  check(minimum_balance_to_create_proposals.symbol.code().to_string() == SUPPORTED_TOKEN, "invalid token code");
+  check(proposal_cost.symbol.code().to_string() == SUPPORTED_TOKEN, "invalid token code");
 
   params_table _params(get_self(), get_self().value);
   auto data = _params.get_or_create(get_self());
-  data.vote_threshold = vote_threshold;
   data.funding_account = funding_account;
+  data.vote_threshold = vote_threshold;
   data.voting_days = voting_days;
+  data.minimum_balance_to_create_proposals = minimum_balance_to_create_proposals;
   data.proposal_cost = proposal_cost;
   data.approver = approver;
   _params.set(data, get_self());
 }
 
-ACTION btclgovernan::clear()
-{
+ACTION btclgovernan::create(name creator, name receiver, name name, string title, string detail, asset amount, string url) {
+  // ask permission of creator account
+  require_auth(creator);
+
+  // validate the token code
+  check(amount.symbol.code().to_string() == SUPPORTED_TOKEN, "invalid token code");
+
+  // validate that there is no proposal with the same name
+  proposals_table _proposals(get_self(), get_self().value);
+  auto _proposal = _proposals.find(name.value);
+  check(_proposal == _proposals.end(), "name already in use");
+
+  // validate the minimum balance of the creator account
+  check(is_valid_minimum_balance(creator), "not enough balance");
+  
+
+  // get the params data
+  params_table _params(get_self(), get_self().value);
+  auto params_data = _params.get_or_create(get_self());
+
+  // save the proposal as a draft
+  _proposals.emplace(get_self(), [ & ](auto & ref) {
+    ref.creator = creator;
+    ref.receiver = receiver;
+    ref.name = name;
+    ref.title = title;
+    ref.detail = detail;
+    ref.amount = amount;
+    ref.url = url;
+    ref.created_on = current_time_point();
+    ref.expires_on = current_time_point() + days(params_data.voting_days);
+    ref.votes_for = 0;
+    ref.votes_against = 0;
+    ref.status = proposal_status::DRAFT;
+  });
+}
+
+ACTION btclgovernan::clear() {
   require_auth(get_self());
+}
+
+bool btclgovernan::is_valid_minimum_balance (name account) {
+  account_table _accounts(eosio::name(EOSIO_TOKEN_CONTRACT), account.value);
+  params_table _params(get_self(), get_self().value);
+  auto params_data = _params.get_or_create(get_self());
+  bool valid = false;
+  
+  for (auto it = _accounts.begin(); it != _accounts.end(); it++)
+  {
+    if (it->balance.symbol.code().to_string() == SUPPORTED_TOKEN && it->balance.amount >= params_data.minimum_balance_to_create_proposals.amount) {
+      valid = true;
+      break;
+    }
+  }
+
+  return valid;
 }
